134c8b2a9f2df6d8ab66e309d295a93f572aad99
novyjpolzovatel <janagur@inbox.ru>
2022-12-15 20:35:29 +0300

    add brush tool

--- client/src/tools/Brush.js
+++ client/src/tools/Brush.js
@@ -12,12 +12,21 @@ export default class Brush extends Tool {
         this.canvas.onmouseup = this.mouseUpHandler.bind(this)
     }
     mouseUpHandler(e){
-
+this.mouseDown = false
     }
     mouseDownHandler(e){
-
+this.mouseDown = true
+this.ctx.beginPath()
+this.ctx.moveTo(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop)
     } 
     mouseMoveHandler(e){
-        
+if(this.mouseDown){
+    this.draw(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop)
+}
+    }
+    draw(x, y){
+        this.ctx.lineTo(x, y)
+        this.ctx.stroke()
+        console.log('draw brush')
     }
 }
